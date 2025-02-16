c995ddc784b25d0009e8b351fe24edefefccbdfa
VKKashyap <vkkashyap@gmail.com>
2025-02-17 02:04:23 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import module1
+import module2
+from utils.helper import greet
+
+print("Running main.py")
+module1.say_hello()
+module2.add_numbers(5, 10)
+
+print(greet("Alice"))
---
+++ module2.py
@@ -0,0 +1,3 @@
+def add_numbers(a, b):
+    print(f"Sum: {a + b}")
+    
